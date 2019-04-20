6a0440761d5b39109ec25a41f3fe2204b2e5f54d
TMT <talaatmedhat19@gmail.com>
2019-04-20 16:48:42 +0200

    sidemeu design

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -1,20 +1,61 @@
-import {React , StyleSheet} from '../includes/CommonImports';
-import  {View , Text} from 'react-native';
+import {React, StyleSheet} from '../includes/CommonImports';
+import {View, Text, TouchableOpacity} from 'react-native';
 import {DARK_COLOR, LIGHT_COLOR} from "../includes/colors";
 
 const SideMenu = (props) => {
     return (
         <View style={styles.container}>
-            <Text style={{color:DARK_COLOR}}>Hello drawer</Text>
-
+            <Text style={styles.header_text}>Doctor Car</Text>
+            <View>
+                <TouchableOpacity>
+                    <Text style={styles.links}>Dashboard</Text>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <Text style={styles.links}>Daily Cycle History</Text>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <Text style={styles.links}>Where Am I ?</Text>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <Text style={styles.links}>Notification</Text>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <Text style={styles.links}>Help Me!</Text>
+                </TouchableOpacity>
+            </View>
+            <View style={styles.footer}>
+                <Text style={{color: LIGHT_COLOR, fontSize: 16}}>
+                    Doctor Car is the most stable system that can track your car state instantly without any mistake
+                    with doctor car you are safe
+                </Text>
+            </View>
         </View>
     )
 };
 
 const styles = StyleSheet.create({
-    container:{
-        width : "50%" ,
-        backgroundColor: LIGHT_COLOR
+    container: {
+        width: "65%",
+        backgroundColor: DARK_COLOR,
+        padding: 10,
+        flex: 1
+    },
+    header_text: {
+        color: LIGHT_COLOR,
+        fontSize: 32,
+        fontWeight: "bold",
+        paddingTop: 20
+    },
+    links: {
+        color: LIGHT_COLOR,
+        fontSize: 20,
+        padding: 15,
+        paddingTop: 25
+    },
+    footer: {
+        display: "flex",
+        flexDirection: "column-reverse",
+        flex: 1
     }
 });
 export default SideMenu;
